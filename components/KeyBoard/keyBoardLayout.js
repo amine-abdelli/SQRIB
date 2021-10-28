e7eae03b556eeca4f18a0d5db9b9dd76db71cdd3
Amine Abdelli <amine.abdelli@outlook.fr>
2021-10-28 16:59:54 +0200

    feat(keyboard): Create keyboard layout

---
+++ components/KeyBoard/keyBoardLayout.js
@@ -0,0 +1,16 @@
+export const keyBoardLayout={
+  'shift-qwerty': [
+    '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+    '{tab} KeyQ KeyW KeyE KeyR KeyT KeyY KeyU KeyI KeyO KeyP { } |',
+    '{lock} KeyA KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL : " Enter',
+    '{shift} KeyZ KeyX KeyC KeyV KeyB KeyN KeyM < > ? {shift}',
+    '.com @ Space'
+  ],
+  'default-azerty': [
+    '² & é " \' \( _ è - ç à ) = {bksp}',
+    '{tab} KeyA KeyZ KeyE KeyR KeyT KeyY KeyU KeyI KeyO KeyP ^ $',
+    '{lock} KeyQ KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL KeyM ù * Enter',
+    '{shift} KeyW KeyX KeyC KeyV KeyB KeyN , ; : ! {shift}',
+    'ControlLeft AltLeft Space AltRight'
+  ]
+}

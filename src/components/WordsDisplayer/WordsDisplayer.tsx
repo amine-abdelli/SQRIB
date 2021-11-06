ce1375ebd7e04a289a484d9b3048fd8255f7d522
Amine Abdelli <amine.abdelli@outlook.fr>
2021-11-06 14:03:06 +0100

    feat(displayer): Create displayer component

---
+++ src/components/WordsDisplayer/WordsDisplayer.tsx
@@ -0,0 +1,47 @@
+import React, { ReactElement } from 'react';
+import { setComputedWordsColor, splitStringToSpans } from '../../helpers/displayer.helper';
+import { WordsDisplayerProps } from './WordsDisplayer.interfaces';
+import styles from './WordsDisplayer.module.scss';
+
+const WordsDisplayer = ({ wordsToDisplay, userInput, wordIndex, setYNextPosition, offSet, setYFocusedPosition, computedWords, fontSize }: WordsDisplayerProps) => {
+  const displayedWords = wordsToDisplay?.map((word: string, i: number): ReactElement => {
+    if (i === wordIndex) {
+      return (
+        <div
+          className={styles.overlay}
+        >
+          <span ref={(element: any) => setYFocusedPosition(element?.getBoundingClientRect().y)} className={styles.wordFocus}>
+            {splitStringToSpans(word, userInput)}
+          </span>
+        </div>);
+    }
+    if (i === wordIndex + 1) {
+      return (
+        <div
+          className={styles.overlay}
+        >
+          <span ref={(element: any) => setYNextPosition(element?.getBoundingClientRect().y)}>
+            {` ${word} `}
+          </span>
+        </div>);
+    }
+    return (
+      <span
+        key={i + Date.now()}
+        style={{ color: setComputedWordsColor(word, i, wordIndex, computedWords)}}
+      >
+        {` ${word} `}
+      </span>
+    );
+    });
+  return (
+    <div className={styles.wordsDisplayer} >
+      <div className={styles.borderTop} />
+      <div style={{ transform: `translate(0, ${offSet}px`, fontSize: fontSize}} className="content">
+        {displayedWords}
+      </div>
+    </div>
+  );
+};
+
+export { WordsDisplayer };

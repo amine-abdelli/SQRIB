2c54eb10e25d13d760f515ac4578a05cd08ccb21
Amine Abdelli <amine.abdelli@outlook.fr>
2021-11-06 14:02:22 +0100

    feat(theme): Implement theme file

---
+++ styles/theme.ts
@@ -0,0 +1,15 @@
+export const themes = {
+  DARK: {
+    theme: 'dark',
+    primary: '#343434',
+    secondary: '#dfdad2',
+    tertiary: '#FF8C00'
+
+  },
+  LIGHT: {
+    theme: 'light',
+    primary: '#dfdad2',
+    secondary: '#343434',
+    tertiary: '#FF8C00'
+  }
+}

5ea69afcc520647b39164f697ef39f7160d5ea81
Amine Abdelli <amine.abdelli@outlook.fr>
2021-11-06 14:07:45 +0100

    feat(repertory): Reorganize folders

--- pages/helpers/countDown.helper.ts
+++
@@ -1,9 +0,0 @@
-import { ICountUpValues } from "../../components/CountDown/CountDown.interfaces";
-
-function formatTimerParameters(countUpValues: ICountUpValues) {
-  const minutes = `0${countUpValues.minutes}`
-  const seconds = `${countUpValues.seconds < 10 ? '0' + countUpValues.seconds : countUpValues.seconds}`
-  return `${minutes}:${seconds}`;
-}
-
-export { formatTimerParameters };

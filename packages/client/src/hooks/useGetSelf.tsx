cf9052f1748448fcaa05cd38a5d77df8cf5684d6
Amine Abdelli <amine.abdelli@outlook.fr>
2021-12-09 21:30:27 +0100

    feat(auth): Create self hook related to authentication

---
+++ packages/client/src/hooks/useGetSelf.tsx
@@ -0,0 +1,13 @@
+import { QueryHookOptions, useQuery } from '@apollo/client';
+import { SELF_QUERY } from '@aqac/api';
+
+export function useGetSelf(options: QueryHookOptions = {}) {
+  const { data, ...rest } = useQuery(SELF_QUERY, {
+    onError: (error) => { console.log('error'); },
+    errorPolicy: 'ignore', // we don't want the app to crash because of that trivial query
+    // instead it will just redirect to login
+    ...options,
+  });
+  const isLoggedIn = Boolean(data?.self.email);
+  return { data, isLoggedIn, ...rest };
+}

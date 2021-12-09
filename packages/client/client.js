5d2cccb5c5dc2784cb26ca1b0075a9fd95d61a0f
Amine Abdelli <amine.abdelli@outlook.fr>
2021-12-09 21:23:40 +0100

    config

---
+++ packages/client/client.js
@@ -0,0 +1,20 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  from,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  // /!\ the order of links is important here, see : https://www.apollographql.com/docs/react/api/link/introduction/#handling-a-response
+  // "HttpLink and BatchHttpLink are both examples of terminating links."
+  link: from([httpLink]),
+  cache: new InMemoryCache(),
+});
+
+export { client };
--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -10,6 +10,37 @@ module.exports = {
     ];
   },
 };
+// const withLess = require('next-with-less');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path = require('path');
+
+// const pathToLessFileWithVariables = path.resolve(
+// 'styles',
+// );
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withLess = require('@zeit/next-less');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withCSS = require('@zeit/next-css');
+
+// module.exports = withCSS(
+//   withLess({
+//     webpack(config) {
+//       return config;
+//     },
+//   }),
+// );
+
+module.exports = withCSS(withLess({
+  lessLoaderOptions: {
+    lessOptions: {
+      modifyVars: {
+        '@primary-color': '#red',
+        '@border-radius-base': '50px',
+      },
+    },
+  },
+}));
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require('next-transpile-modules')(['@aqac/utils', '@aqac/api', '@aqac/client']);
 

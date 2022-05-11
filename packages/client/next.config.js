1ba809bdfe5fc516a0e4f218e39929359381b6f7
Amine Abdelli <amine.abdelli@outlook.fr>
2022-05-11 23:24:56 +0200

    chore(next.config): Remove less config

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -12,21 +12,6 @@ module.exports = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withLess = require('@zeit/next-less');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withCSS = require('@zeit/next-css');
-
-module.exports = withCSS(withLess({
-  lessLoaderOptions: {
-    lessOptions: {
-      modifyVars: {
-        '@primary-color': '#red',
-        '@border-radius-base': '50px',
-      },
-    },
-  },
-}));
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require('next-transpile-modules')(['@aqac/utils', '@aqac/api', '@aqac/client']);
 
 module.exports = withTM();

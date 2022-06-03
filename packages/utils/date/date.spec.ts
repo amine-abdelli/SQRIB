b82c22a2ad8d0bdf0432de7c8231fc1eb297fda4
Amine Abdelli <amine.abdelli@outlook.fr>
2022-06-03 14:48:12 +0200

    feat: Update test on dates functions

--- packages/utils/date/date.spec.ts
+++ packages/utils/date/date.spec.ts
@@ -6,6 +6,10 @@ describe('Should format date', () => {
     expect(formatDateToCalendar(date)).toBe('2022-03-30');
   });
 
+  it('Should format the date object to this format: DD/MM/YYY', () => {
+    expect(formatDateToCalendar(date)).toBe('30/03/2022');
+  });
+
   it('Should format the date object to this format: lun. 01 janvier 2000', () => {
     expect(formatDate(date, 'fr')).toBe('mer. 30 mars 2022');
     expect(formatDate(date, 'en')).toBe('Wed, 30 March 2022');

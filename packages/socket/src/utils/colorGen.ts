4cc5e40f4ec9e602c67c0f7b089a8b4fca87da48
Amine Abdelli <amine.abdelli@outlook.fr>
2022-08-22 23:32:52 +0200

    feat(utils): Migrate utils to utils folder

--- packages/socket/src/utils/colorGen.ts
+++ packages/socket/src/utils/colorGen.ts
@@ -1,3 +1,7 @@
+/**
+ * Generate a random color
+ * @returns {string} An hex color
+ */
 function colorGenerator() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return `#${randomColor}`;

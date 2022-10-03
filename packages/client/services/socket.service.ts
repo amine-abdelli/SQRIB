4654157006c1caa89e35562773c38bfdbbca0fea
amine-abdelli <amine.abdelli@outlook.fr>
2022-10-03 14:02:08 +0200

    chore(monorepo): Dockerize sqrib

--- packages/client/services/socket.service.ts
+++ packages/client/services/socket.service.ts
@@ -1,6 +1,6 @@
 import io, { Socket } from 'socket.io-client';
 
-const ENDPOINT = process.env.SOCKET_URL || 'ws://localhost:4001';
+const ENDPOINT = process.env.SOCKET_URL || 'http://localhost:4001';
 const socket = io(ENDPOINT, {
   transports: ['websocket'],
   autoConnect: false,
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -15,16 +15,18 @@ const socketServer = http.createServer(app_socket);
 const io = require('socket.io')(socketServer, {
   cors: {
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    credentials: true,
+    // methods: ['GET', 'POST'],
+    // credentials: true,
   },
 });
 
 dotenv.config();
-const PORT: string | 4000 = process.env.PORT || 4000;
+const APOLLO_SERVER_PORT = process.env.APOLLO_SERVER_PORT || 4000;
+const SOCKET_SERVER_PORT = process.env.SOCKET_SERVER_PORT || 4001;
 
 async function startServer() {
   const app = express();
+  app.use(cors())
   app_socket.use(cors());
   initializeSocket(io);
   const apolloServer: ApolloServer<any> = new ApolloServer({
@@ -51,9 +53,9 @@ async function startServer() {
     res.send('Hello from express apollo server');
   });
 
-  app.listen(PORT, () => log.info(`Server is running on port ${PORT}`));
+  app.listen(APOLLO_SERVER_PORT, () => log.info(`Apollo server running on port ${APOLLO_SERVER_PORT}`));
   // Socket server
-  socketServer.listen(4001, () => log.info('listening on *:4001'));
+  socketServer.listen(SOCKET_SERVER_PORT, () => log.info(`Websocket server running on port ${process.env.SOCKET_SERVER_PORT}`));
 }
 
 startServer();

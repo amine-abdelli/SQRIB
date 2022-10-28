7e9ae59910bae3f902ca9ae041a9f19ec74694ac
amine-abdelli <amine.abdelli@outlook.fr>
2022-10-28 17:39:14 +0200

    feat(UI/table): Create columns hook to adapt tables

---
+++ packages/client/src/components/LeaderBoard/useColumns.hook.tsx
@@ -0,0 +1,144 @@
+import { Hide, Show } from 'react-iconly';
+// import { theme } from '../../../styles/theme';
+
+export function useColumns() {
+  // For all the players
+  function soloColumns(fn?: any) {
+    return [
+      {
+        title: 'Position', // What is displayed by in the table header
+        dataIndex: 'position', // The key corresponding to our data
+        key: 'position',
+      },
+      {
+        title: 'Nom', // What is displayed by in the table header
+        dataIndex: 'username', // The key corresponding to our data
+        key: 'name',
+      },
+      {
+        title: 'Mpm', // What is displayed by in the table header
+        dataIndex: 'mpm', // The key corresponding to our data
+        key: 'mpm',
+      },
+      {
+        title: 'Precision', // What is displayed by in the table header
+        dataIndex: 'precision', // The key corresponding to our data
+        key: 'victory',
+      },
+      {
+        title: 'Date', // What is displayed by in the table header
+        dataIndex: 'date', // The key corresponding to our data
+        key: 'date',
+      },
+      {
+        title: '', // What is displayed by in the table header
+        dataIndex: 'userId', // The key corresponding to our data
+        key: 'userId',
+        render: (item: any, currentData: any) => (currentData.username && (
+          item ? <span onClick={() => fn(item)}><Show style={{ margin: 0, cursor: 'pointer' }} /></span> : <Hide />
+        )),
+      },
+    ];
+  }
+  // For all the players
+  function multiplayerColumns(fn?: any) {
+    return [
+      {
+        title: 'Position', // What is displayed by in the table header
+        dataIndex: 'position', // The key corresponding to our data
+        key: 'position',
+      },
+      {
+        title: 'Nom', // What is displayed by in the table header
+        dataIndex: 'username', // The key corresponding to our data
+        key: 'name',
+      },
+      {
+        title: 'Mpm', // What is displayed by in the table header
+        dataIndex: 'mpm', // The key corresponding to our data
+        key: 'mpm',
+      },
+      {
+        title: 'Victoires', // What is displayed by in the table header
+        dataIndex: 'victory', // The key corresponding to our data
+        key: 'victory',
+      },
+      {
+        title: 'Date', // What is displayed by in the table header
+        dataIndex: 'date', // The key corresponding to our data
+        key: 'date',
+      },
+      {
+        title: '', // What is displayed by in the table header
+        dataIndex: 'userId', // The key corresponding to our data
+        key: 'userId',
+        render: (item: any, currentData: any) => (currentData.username && (
+          item ? <span onClick={() => fn(item)}><Show style={{ margin: 0, cursor: 'pointer' }} /></span> : <Hide />
+        )),
+      },
+    ];
+  }
+
+  function multiplayerDetails(fn?: any) {
+    return [
+      {
+        title: 'Precision', // What is displayed by in the table header
+        dataIndex: 'precision', // The key corresponding to our data
+        key: 'precision',
+      },
+      {
+        title: 'Mpm', // What is displayed by in the table header
+        dataIndex: 'mpm', // The key corresponding to our data
+        key: 'name',
+      },
+      {
+        title: 'Victoire', // What is displayed by in the table header
+        dataIndex: 'hasWon', // The key corresponding to our data
+        key: 'position',
+      },
+      {
+        title: 'Langue', // What is displayed by in the table header
+        dataIndex: 'language', // The key corresponding to our data
+        key: 'mpm',
+      },
+      {
+        title: 'Date', // What is displayed by in the table header
+        dataIndex: 'date', // The key corresponding to our data
+        key: 'date',
+      },
+    ];
+  }
+
+  function soloDetails(fn?: any) {
+    return [
+      {
+        title: 'Mpm', // What is displayed by in the table header
+        dataIndex: 'mpm', // The key corresponding to our data
+        key: 'name',
+      },
+      {
+        title: 'Precision', // What is displayed by in the table header
+        dataIndex: 'precision', // The key corresponding to our data
+        key: 'precision',
+      },
+      {
+        title: 'Points', // What is displayed by in the table header
+        dataIndex: 'points', // The key corresponding to our data
+        key: 'points',
+      },
+      {
+        title: 'Langue', // What is displayed by in the table header
+        dataIndex: 'language', // The key corresponding to our data
+        key: 'mpm',
+      },
+      {
+        title: 'Date', // What is displayed by in the table header
+        dataIndex: 'date', // The key corresponding to our data
+        key: 'date',
+      },
+    ];
+  }
+  return {
+    multiplayerColumns, soloColumns, multiplayerDetails, soloDetails,
+  };
+}

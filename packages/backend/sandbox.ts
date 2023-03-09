f80027effc3256106e9c2c9a0ed6938eaf12cd64
amine-abdelli <amine.abdelli@outlook.fr>
2023-03-10 00:32:13 +0100

    chore(yarn-workspace): run lint, test, build and start

--- packages/backend/sandbox.ts
+++ packages/backend/sandbox.ts
@@ -1,10 +1,7 @@
-import { prisma } from './src/client';
-
 (async () => {
   try {
     //
   } catch (e) {
     console.log('an error occurred', e);
   }
-  await prisma.$disconnect();
 })();
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react-swc';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({

c26620b2b86779ec12ea62841a566d62a749d731
Amine Abdelli <amine.abdelli@outlook.fr>
2023-03-30 15:12:04 +0200

    chore(logs): Update logger

--- packages/backend/src/utils/log.utils.ts
+++ packages/backend/src/utils/log.utils.ts
@@ -1,6 +1,6 @@
 import pino from 'pino';
 
-const log = pino(
+const baseLogger = pino(
   {
     level: process.env.LOG_LEVEL || 'info',
     transport: {
@@ -12,5 +12,13 @@ const log = pino(
     },
   },
 );
+const logWrapper = {
+  info: (msg: string, obj: Record<string, string> = {}) => baseLogger.info(obj, msg),
+  error: (msg: string, obj: Record<string, string> = {}) => baseLogger.error(obj, msg),
+  warn: (msg: string, obj: Record<string, string> = {}) => baseLogger.warn(obj, msg),
+  debug: (msg: string, obj: Record<string, string> = {}) => baseLogger.debug(obj, msg),
+  fatal: (msg: string, obj: Record<string, string> = {}) => baseLogger.fatal(obj, msg),
+  trace: (msg: string, obj: Record<string, string> = {}) => baseLogger.trace(obj, msg),
+};
 
-export { log };
+export { logWrapper as log };

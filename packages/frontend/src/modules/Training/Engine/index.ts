374d6028760bccda000a334dd9d1bdd32af2a861
amine-abdelli <amine.abdelli@outlook.fr>
2023-05-29 22:07:23 +0200

    feat(training): create training module engine

--- packages/frontend/src/modules/Training/Engine/index.ts
+++ packages/frontend/src/modules/Training/Engine/index.ts
@@ -1,1 +1,2 @@
 export * from './Engine.component';
+export * from './Engine.props';
---
+++ packages/frontend/src/modules/Training/Training.module.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  EngineProps, Spacer, SpacerSize, WordsCollection,
+} from '../../components';
+import { TypingInput } from '../../components/TypingInput';
+import { Options } from '../../components/Options/Options.component';
+import { WordsCollectionHeader } from '../../components/DisplayerHeader/DisplayerHeader.component';
+import { Scoring } from '../../components/Scoring/Scoring.component';
+import { WordsCollectionLayout } from '../../components/Options/Options.props';
+import { Button } from '../../components/Button/Button.component';
+import KeyBoard from '../../components/KeyBoard/KeyBoard.component';
+import '../../theme/components/_containers.scss';
+import { TrainingModal } from './components/TrainingModal/TrainingModal.component';
+
+function TrainingModule(props: EngineProps) {
+  return (
+    <section className='training-container--wrapper'>
+      <Options {...props} />
+      <Scoring {...props} />
+      <Spacer size={SpacerSize.LARGE} y />
+      <WordsCollectionHeader {...props} />
+      <div style={{ position: 'relative' }}>
+        {!props.isRunning && <Button style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: 0 }} onClick={() => props.resetTraining()} label="Replay" stretch />}
+        {!props.isRunning && <Button style={{ display: 'flex', justifyContent: 'flex-end' }} onClick={() => props.resetTrainingAndRefetch()} label="New game" stretch />}
+        {props.isRunning && <Button style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: 0, background: 'red' }} onClick={() => props.resetTraining()} label="STOP" stretch />}
+        <TypingInput {...props} />
+      </div>
+      <Spacer size={SpacerSize.MEDIUM} y />
+      {props.layout === WordsCollectionLayout.HORIZONTAL
+        ? <WordsCollection {...props} /> : <WordsCollection {...props} />}
+      <KeyBoard enable />
+      <TrainingModal {...props} />
+    </section>
+  );
+}
+
+export { TrainingModule };

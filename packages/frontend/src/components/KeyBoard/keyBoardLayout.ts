18d9ffd4b9ff16d9b70071f762213da51936adfb
amine-abdelli <amine.abdelli@outlook.fr>
2023-08-28 01:51:54 +0200

    feat(keyboard): change keyboard layout

--- packages/frontend/src/components/KeyBoard/keyBoardLayout.ts
+++ packages/frontend/src/components/KeyBoard/keyBoardLayout.ts
@@ -1,16 +1,52 @@
 export const keyBoardLayout = {
   'default-qwerty': [
-    '~ ! @ # $ % ^ & * ( ) _ + Backspace',
-    'Tab q w e r t y u i o p { } |',
-    'CapsLock a s d f g h j k l : " Enter',
-    'ShiftLeft z x c v b n m < > ? ShiftRight',
+    '` 1 2 3 4 5 6 7 8 9 0 - = Backspace',
+    'Tab q w e r t y u i o p [ ] \\',
+    'CapsLock a s d f g h j k l ; \' Enter',
+    'ShiftLeft z x c v b n m , . / ShiftRight',
     'ControlLeft AltLeft Space AltRight',
   ],
   'default-azerty': [
-    'Escape & é " \' ( _ è - ç à ) = Backspace',
-    'Tab a z e r t y u i o p ^ $',
-    'CapsLock q s d f g h j k l m ù * Enter',
-    'ShiftLeft w x c v b n , ; : ! ShiftRight',
+    '² & é " \' ( - è _ ç à ) = Backspace',
+    'Tab a z e r t y u i o p ^ $ *',
+    'CapsLock q s d f g h j k l m ù Enter',
+    'ShiftLeft < w x c v b n , ; : ! ShiftRight',
     'ControlLeft AltLeft Space AltRight',
   ],
 };
+
+// export const keyBoardLayout = {
+//   'default-qwerty': {
+//     'normal': [
+//       '` 1 2 3 4 5 6 7 8 9 0 - = Backspace',
+//       'Tab q w e r t y u i o p [ ] \\',
+//       'CapsLock a s d f g h j k l ; \' Enter',
+//       'ShiftLeft z x c v b n m , . / ShiftRight',
+//       'ControlLeft AltLeft Space AltRight',
+//     ],
+//     'maj': [
+//       '~ ! @ # $ % ^ & * ( ) _ + Backspace',
+//       'Tab Q W E R T Y U I O P { } |',
+//       'CapsLock A S D F G H J K L : " Enter',
+//       'ShiftLeft Z X C V B N M < > ? ShiftRight',
+//       'ControlLeft AltLeft Space AltRight',
+//     ],
+//   },
+//   'default-azerty': {
+//     'normal': [
+//       '² & é " \' ( - è _ ç à ) = Backspace',
+//       'Tab a z e r t y u i o p ^ $ *',
+//       'CapsLock q s d f g h j k l m ù Enter',
+//       'ShiftLeft < w x c v b n , ; : ! ShiftRight',
+//       'ControlLeft AltLeft Space AltRight',
+//     ],
+//     'maj': [
+//       '² 1 2 3 4 5 6 7 8 9 0 ° + Backspace',
+//       'Tab A Z E R T Y U I O P ¨ £ µ',
+//       'CapsLock Q S D F G H J K L M % Enter',
+//       'ShiftLeft > W X C V B N ? . / § ShiftRight',
+//       'ControlLeft AltLeft Space AltRight',
+//     ],
+//   },
+// };
+
--- packages/frontend/src/modules/Training/components/SettingsModal/SettingsModal.component.tsx
+++ packages/frontend/src/modules/Training/components/SettingsModal/SettingsModal.component.tsx
@@ -20,7 +20,7 @@ import { ModeOptionGroup, OptionGroup } from './SubComponents/OptionGroup';
 function ModeOptions({ mode, setMode, countDown, setCountDown, wordCount, setWordCount, isZenModeOn, setIsZenModeOn }: SettingsModalProps) {
   const [wordsType, setWordsType] = useState<TWordsType>(WordsType.RANDOM);
 
-  const modeOptions = [{ value: TrainingMode.TIME_TRIAL, label: 'Time Trial', subLabel: 'Race against the clock and type as many words as possible in your chosen timeframe.' }, { value: TrainingMode.SPEED_CHALLENGE, label: 'Speed Challenge', subLabel: 'Type as many words as possible in your chosen timeframe.' }];
+  const modeOptions = [{ value: TrainingMode.TIME_TRIAL, label: 'Time Trial', subLabel: 'Race against the clock and type as many words as possible in your chosen timeframe.' }, { value: TrainingMode.SPEED_CHALLENGE, label: 'Speed Challenge', subLabel: 'Choose a word count and type it out as quickly as you can.' }];
   const wordCountOptions = [{ value: 25, label: 25 }, { value: 50, label: 50 }, { value: 75, label: 75 }, { value: 100, label: 100 }, { value: 125, label: 125 }, { value: 150, label: 150 }];
   const durationOptions = [{ value: 15, label: 15 }, { value: 30, label: 30 }, { value: 60, label: 60 }, { value: 75, label: 75 }, { value: 90, label: 90 }, { value: 120, label: 120 }];
   const wordTypeOptions = [{ value: 'random', label: 'random' }, { value: 'quote', label: 'quote' }, { value: 'custom', label: 'custom' }];

7d5f830c74ab134eaa99b2adcd397e13b353ccab
amine-abdelli <amine.abdelli@outlook.fr>
2023-09-12 23:03:37 +0200

    chore(whitelist): remove local ips

--- packages/backend/src/middlewares/access.middleware.ts
+++ packages/backend/src/middlewares/access.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export function accessMiddleware(req: Request, res: Response, next: NextFunction) {
   const clientIp = req.ip; // Get client IP
-  const allowedIps = ['127.0.0.1', '192.168.1.1', '18.156.158.53', '18.156.42.200', '52.59.103.54', '::1']; // Replace with your list of IPs
+  const allowedIps = ['18.156.158.53', '18.156.42.200', '52.59.103.54']; // Replace with your list of IPs
   if (process.env.NODE_ENV !== 'development') {
     if (allowedIps.includes(clientIp)) {
       next();
